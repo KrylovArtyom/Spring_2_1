c6598dd204b7b7d0636109b83255fc67536c7423
KrylovArtyom <88318874+KrylovArtyom@users.noreply.github.com>
2021-08-27 14:43:37 +0300

    333

--- 2_1_2/src/main/java/app/model/AnimalsCage.java
+++ 2_1_2/src/main/java/app/model/AnimalsCage.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 public class AnimalsCage {
 
     @Autowired
-    @Qualifier("dog")
+    @Qualifier ("dog")
     private Animal animal;
 
     @Autowired
--- 2_1_3/src/main/java/koschei/config/AppConfig.java
+++ 2_1_3/src/main/java/koschei/config/AppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "koschei")
+@ComponentScan (basePackages = "koschei")
 public class AppConfig {
 
     @Bean
